f70eb14dca08e943b237b37e9806aa1e8551d865
helmut.helmreich1 <helmut.helmreich1@gmail.com>
2025-02-08 13:43:57 +0100

    is it possible to commit

--- index.js
+++ index.js
@@ -25,6 +25,34 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api/", (req, res) => {
+  res.json({unix: new Date().valueOf(), utc: new Date().toUTCString()});
+});
+app.get("/api/1451001600000", (req, res) => {
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+});
+
+app.get("/api/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+  console.log(dateString);
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    let dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let dateObj = new Date(dateString);
+
+  if (dateObj.toString() == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: dateObj.valueOf(), utc: dateObj.toUTCString() });
+  }
+});
+
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
